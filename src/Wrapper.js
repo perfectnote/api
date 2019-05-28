ea64911153396bd4578c0b6a7cf3e22e0b041a6e
Aiden Bai <mail.contraband@gmail.com>
2019-05-28 07:49:41 -0700

    Made a wrapper around the express app, double quotes standard => single quotes. Code cleanup

---
+++ src/Wrapper.js
@@ -0,0 +1,40 @@
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+import apiSetup from './api/v1/api';
+
+dotenv.config();
+const app = express();
+
+module.exports = class Wrapper {
+  constructor(options) {
+    this.options = options;
+  }
+
+  start(port = this.options.port || process.env.PORT || 5000) {
+    app.disable('x-powered-by');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(require('express-bearer-token')());
+
+    // Setup API
+    app.use('/api/v1', apiSetup(express.Router()));
+
+    app.listen(port, () => {
+      this.log(`Listening on port ${port}`, 'API');
+    });
+  }
+
+  log(...args) {
+    const message = args[0];
+    const tags = args.slice(1).map((tag) => `[${tag}]`);
+    console.log(...tags, message);
+  }
+
+  logError(...args) {
+    const tags = args.length > 1 
+      ? args.slice(0, -1).map((tags) => `[${tags}]`) 
+      : [];
+    console.error('[Error]', ...tags, args[args.length - 1]);
+  }
+};
