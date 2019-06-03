4a0531572632e0f2cf9ca8ee9ebfc171646398a9
Aiden Bai <mail.contraband@gmail.com>
2019-06-02 18:52:14 -0700

    Organize content

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-<a href="https://travis-ci.org/perfectnote/api"><img src="https://img.shields.io/travis/perfectnote/api.svg?style=flat-square"/></a>
-<a href="https://david-dm.org/perfectnote/api"><img src="https://david-dm.org/perfectnote/api/status.svg?style=flat-square"/></a>
-<a href="https://david-dm.org/perfectnote/api?type=dev"><img src="https://david-dm.org/perfectnote/api/dev-status.svg?style=flat-square"/>
-
-# PerfectNote API
+PerfectNote API
+====
+[![](https://img.shields.io/travis/perfectnote/api.svg?style=flat-square)](https://travis-ci.org/perfectnote/api) [![](https://david-dm.org/perfectnote/api/status.svg?style=flat-square)](https://david-dm.org/perfectnote/api) [![](https://david-dm.org/perfectnote/api/dev-status.svg?style=flat-square)](https://david-dm.org/perfectnote/api?type=dev)
--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,19 +1,19 @@
 import dotenv from 'dotenv';
-
 import express from 'express';
-import apiSetup from './api/v1/api';
-
-import log from './utils/Logger.js';
-import connect from './db/connection.js';
-import { Post } from './models';
 
 import bodyParser from 'body-parser';
 import tokenBearer from 'express-bearer-token';
 import cors from 'cors';
 
+import morgan from 'morgan';
+import log from './utils/Logger.js';
+import connect from './db/connection.js';
+
+import apiRoute from './api/v1/api.js';
+
 dotenv.config();
 const app = express();
-const db = connect(process.env.DB_URL);
+const db = connect(process.env.DB_URL, { useNewUrlParser: true });
 
 export default class Wrapper {
   constructor(options = {}) {
@@ -23,12 +23,13 @@ export default class Wrapper {
 
   start(port = this.options.port || process.env.PORT || 5000) {
     app.disable('x-powered-by');
+    app.use(morgan('combined'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(tokenBearer());
     app.use(cors());
+    app.use(tokenBearer());
   
-    app.use('/api/v1', apiSetup(express.Router()));
+    app.use('/api/v1', apiRoute(express.Router()));
 
     app.listen(port, () => {
       this.log(`Listening on port ${port}`, 'API');
