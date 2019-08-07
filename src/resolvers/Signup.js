01e8c08f05d0ce8717ad275d8501f01cfa0e380c
Aiden Bai <mail.contraband@gmail.com>
2019-08-06 20:29:44 -0700

    Refactor code to ES6 in Signup.js

--- src/resolvers/Signup.js
+++ src/resolvers/Signup.js
@@ -6,16 +6,13 @@ import { User } from '../models';
 export default async (_, args) => {
   if (!isEmailValid(args.email)) throw new UserInputError('Invalid email');
 
-  var password = await hashPassword(args.password, 10);
+  const password = await hashPassword(args.password, 10);
 
-  var username =
-    args.email.split('@')[0].replace(/\W/g, '-') +
-    '-' +
-    Date.now()
-      .toString(16)
-      .slice(-6);
+  const username = `${args.email.split('@')[0].replace(/\W/g, '-')}-${Date.now()
+    .toString(16)
+    .slice(-6)}`;
 
-  var user = {
+  const user = {
     _id: new mongo.ObjectId(),
     name: args.name,
     email: args.email,
@@ -23,11 +20,11 @@ export default async (_, args) => {
     username,
   };
 
-  let response = await User.create(user);
+  const response = await User.create(user);
 
   return { token: 'test', user: response };
 };
 
-function isEmailValid(email) {
+const isEmailValid = (email) => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
+};
