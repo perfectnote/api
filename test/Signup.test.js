a8772e779a3c5119d508c3291e6efc46456ed8fa
Diogo Correia <me@diogotc.com>
2019-08-09 11:13:14 +0100

    Add token validation to Signup test

--- test/Signup.test.js
+++ test/Signup.test.js
@@ -1,14 +1,15 @@
 import { UserInputError } from 'apollo-server-core';
 import { compare as comparePassword } from 'bcrypt';
 import { expect } from 'chai';
+import jwt from 'jsonwebtoken';
 import mongoUnit from 'mongo-unit';
 import { signup } from '../src/modules/Signup';
-import cleanData from './fixtures/signupFixture.json';
+import signupFixture from './fixtures/signupFixture.json';
 
 const testMongoUrl = process.env.DB_URL;
 
 describe('modules/Signup', () => {
-  before(() => mongoUnit.initDb(testMongoUrl, cleanData));
+  before(() => mongoUnit.initDb(testMongoUrl, signupFixture));
   after(() => mongoUnit.drop());
 
   it('should register a pre-defined user correctly', async () => {
@@ -16,28 +17,24 @@ describe('modules/Signup', () => {
 
     expect(data.token).to.be.a('string', 'token is not a string');
     expect(data.user).to.not.be.a('undefined', "user object can't be undefined");
-    expect(data.user._id).to.be.a('string', 'id is not a string');
+    expect(data.user._id).to.be.a('string', 'id is not a swtring');
     expect(data.user.name).to.be.equal('John Doe', "name doesn't match what was provided");
     expect(data.user.email).to.be.equals(
       'john.doe@example.com',
       "email doesn't match what was provided"
     );
-    expect(
-      !!(await comparePassword('password123456', data.user.password)),
-      'passwords should match'
-    ).to.be.true;
+    await expect(comparePassword('password123456', data.user.password), 'passwords should match').to
+      .eventually.be.true;
     expect(data.user.username).to.match(/john-doe-[\d\w]{6}/, "username isn't correctly setup");
+
+    expect(jwt.verify(data.token, process.env.JWT_SECRET))
+      .to.be.an('object')
+      .that.includes.all.keys('id', 'name', 'username');
   });
 
-  it('should throw error for invalid email', async () => {
-    await expect(signup('john.doe', 'password123456', 'John Doe')).to.be.rejectedWith(
-      UserInputError
-    );
-  });
+  it('should throw error for invalid email', () =>
+    expect(signup('john.doe', 'password123456', 'John Doe')).to.be.rejectedWith(UserInputError));
 
-  it('should throw error for duplicate email', async () => {
-    await expect(signup('john.doe@example.com', 'password123456', 'John Doe')).to.be.rejectedWith(
-      Error
-    );
-  });
+  it('should throw error for duplicate email', () =>
+    expect(signup('john.doe@example.com', 'password123456', 'John Doe')).to.be.rejectedWith(Error));
 });
